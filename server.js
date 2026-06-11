157c215a81c908cac53c8b5262f20e3263ed942e
tavkeer <shahtavkeer@gmail.com>
2026-06-11 16:18:38 +0530

    server.js fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
-simple - node - app
+
 app.get("/", (req, res) => {
   res.send(`
 <!DOCTYPE html>
@@ -38,8 +38,8 @@ h1{
 
 <body>
 <div class="card">
-    <h1>Jenkins CI/CD</h1>
-    <p>Deployment Successful V5 🚀</p>
+    <h1>GitHub Actions CI/CD</h1>
+    <p>Deployment Successful V8🚀</p>
     <p>Node.js running on server</p>
 </div>
 </body>
